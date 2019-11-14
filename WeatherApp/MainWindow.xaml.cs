1f200cdebc0c5e1552ae42d3b9a53c1f278142cb
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-11-14 21:45:46 +0600

    Приложение для проверки погоды

---
+++ WeatherApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WeatherInfo weather = new WeatherInfo();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void updateClick(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://api.openweathermap.org/data/2.5/forecast?q={cityTB.Text}&units=metric&appid=9176f39c6ae3eb96104eb48077805a7a&cnt=5";
+
+            using (WebClient client = new WebClient())
+            {
+                var json = client.DownloadString(url);
+                weather = JsonConvert.DeserializeObject<WeatherInfo>(json);
+            }
+            dataGrid.ItemsSource = weather.List;
+        }
+
+        private void cityTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WeatherApp/WeatherData.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp
+{
+    public partial class WeatherInfo
+    {
+        public long Cod { get; set; }
+        public long Message { get; set; }
+        public long Cnt { get; set; }
+        public List<List> List { get; set; }
+        public City City { get; set; }
+    }
+
+    public partial class City
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public Coord Coord { get; set; }
+        public string Country { get; set; }
+        public long Population { get; set; }
+        public long Timezone { get; set; }
+        public long Sunrise { get; set; }
+        public long Sunset { get; set; }
+    }
+
+    public partial class Coord
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+
+    public partial class List
+    {
+        public long Dt { get; set; }
+        public DateTime asd {get;set;}
+        public Main Main { get; set; }
+        public List<Weather> Weather { get; set; }
+        public Clouds Clouds { get; set; }
+        public Wind Wind { get; set; }
+        public Sys Sys { get; set; }
+        public DateTimeOffset DtTxt { get; set; }
+    }
+
+    public partial class Clouds
+    {
+        public long All { get; set; }
+    }
+
+    public partial class Main
+    {
+        public double Temp { get; set; }
+        public double Temp_Min { get; set; }
+        public double Temp_Max { get; set; }
+        public long Pressure { get; set; }
+        public long SeaLevel { get; set; }
+        public long GrndLevel { get; set; }
+        public long Humidity { get; set; }
+        public double TempKf { get; set; }
+    }
+
+    public partial class Sys
+    {
+        public string Pod { get; set; }
+    }
+
+    public partial class Weather
+    {
+        public long Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public partial class Wind
+    {
+        public double Speed { get; set; }
+        public long Deg { get; set; }
+    }
+
+}
